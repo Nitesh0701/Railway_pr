f06a09d161619fac140b6ab2ccb9517a368da12f
Nitesh0701 <138009171+Nitesh0701@users.noreply.github.com>
2024-02-18 08:34:36 +0530

    Update get_trains.py

--- RESystem/railapp/train_service/get_trains.py
+++ RESystem/railapp/train_service/get_trains.py
@@ -6,7 +6,7 @@ def get_trains(startStationCode:str,endStationCode:str,dateOfJourney:str):
     querystring = {"fromStationCode":startStationCode,"toStationCode":endStationCode,"dateOfJourney":dateOfJourney}
 
     headers = {
-        "X-RapidAPI-Key": "d1a177f3e5mshc45c9a81610c1f9p16b308jsn36e29bdc8ce4",
+        "X-RapidAPI-Key": 
         "X-RapidAPI-Host": "irctc1.p.rapidapi.com"
     }
     response = requests.get(BASE_URL, headers=headers, params=querystring)
